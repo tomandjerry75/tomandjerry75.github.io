81cb69810bf8b690999ba8893cdcc9d18b8841f5
tomandjerry75 <70911941+tomandjerry75@users.noreply.github.com>
2020-10-18 00:49:42 -0400

    Create stopinconsole.js

---
+++ stopinconsole.js
@@ -0,0 +1,36 @@
+console.log('            uuuuuuuuuuuuuuuuuuuu');
+console.log('          u" uuuuuuuuuuuuuuuuuu "u');
+console.log('        u" u$$$$$$$$$$$$$$$$$$$$u "u');
+console.log('      u" u$$$$$$$$$$$$$$$$$$$$$$$$u "u');
+console.log('    u" u$$$$$$$$$$$$$$$$$$$$$$$$$$$$u "u');
+console.log('  u" u$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$u "u');
+console.log('u" u$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$u "u');
+console.log('$ $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ $');
+console.log('$ $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ $');
+console.log('$ $$$" ... "$...  ...$" ... "$$$  ... "$$$ $');
+console.log('$ $$$u `"$$$$$$$  $$$  $$$$$  $$  $$$  $$$ $');
+console.log('$ $$$$$$uu "$$$$  $$$  $$$$$  $$  """ u$$$ $');
+console.log('$ $$$$$$uu "$$$$  $$$  $$$$$  $$  """ u$$$ $');
+console.log('$ $$$""$$$  $$$$  $$$u "$$$" u$$  $$$$$$$$ $');
+console.log('$ $$$$....,$$$$$..$$$$$....,$$$$..$$$$$$$$ $');
+console.log('$ $$$$....,$$$$$..$$$$$....,$$$$..$$$$$$$$ $');
+console.log('$ $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ $');
+console.log('"u "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" u"');
+console.log('  "u "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" u"');
+console.log('    "u "$$$$$$$$$$$$$$$$$$$$$$$$$$$$" u"');
+console.log('      "u "$$$$$$$$$$$$$$$$$$$$$$$$" u"');
+console.log('        "u "$$$$$$$$$$$$$$$$$$$$" u"');
+console.log('          "u """""""""""""""""" u"');
+console.log('            """"""""""""""""""""');
+
+console.log('Please don\'t hack my website.');
+
+console.log('');
+
+console.log('So, if you are on the console, you are probably');
+console.log('here to hack my website or something. I ask you,');
+console.log('please don\'t. No good will come to you.');
+console.log('This is just a fun thing I made because I was bored.');
+console.log('Thanks! I appreciate you a LOT!');
+console.log('If you want the source code, go to my GitHub and you');
+console.log('will see a repository called \"website\"');
